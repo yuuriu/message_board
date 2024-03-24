2e559f78bb9d7ae47df91e4cf02b8baadfebf932
yuuto.ueda <kakegawayuuri@gmail.com>
2024-03-24 17:56:36 +0900

    ページネーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -36,11 +36,21 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated method stub
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        int page = 1;
+
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).setFirstResult(15 * (page - 1)).setMaxResults(15).getResultList();
+
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class).getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count);
+        request.setAttribute("page", page);
 
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
