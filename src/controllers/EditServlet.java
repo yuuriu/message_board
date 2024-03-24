0eeaea070531e09c654ff73137edfea38dc3c77f
yuuto.ueda <kakegawayuuri@gmail.com>
2024-03-24 15:43:07 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -42,6 +42,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id", m.getId());
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        String _token = request.getParameter("_token");
+
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            String title = request.getParameter("title");
+            m.setTitle(title);
+
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);
+
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("message_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+
+        }
+    }
+
+}
