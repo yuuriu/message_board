83469e2fe271fff351e083fb61cd4aa3c95854e3
yuuto.ueda <kakegawayuuri@gmail.com>
2024-03-24 17:38:41 +0900

    フラッシュメッセージを追加

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -41,6 +41,7 @@ public class DestroyServlet extends HttpServlet {
             em.getTransaction().begin();
             em.remove(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "削除が完了しました。");
             em.close();
 
             request.getSession().removeAttribute("message_id");
